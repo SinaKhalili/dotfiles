628228dce4d3041357c4ef890b442b8dddf64ab4
sina <khalili@sfu.ca>
2024-06-09 23:14:11 -0700

    Add tmux

--- scripts/vps-setup.sh
+++ scripts/vps-setup.sh
@@ -1,6 +1,6 @@
 #  this is a setup for simply getting zsh + vim working on a vps
 # === ZSH ===
-apt install zsh
+apt install zsh tmux
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 mkdir ~/.config/antigen
 curl -L git.io/antigen > ~/.config/antigen/antigen.zsh
@@ -8,6 +8,9 @@ curl https://raw.githubusercontent.com/SinaKhalili/dotfiles/master/zsh/.zshrc > 
 
 # === VIM ===
 curl https://raw.githubusercontent.com/SinaKhalili/dotfiles/master/vim/.vimrc > ~/.vimrc
+
+# === TMUX ===
+curl https://raw.githubusercontent.com/SinaKhalili/dotfiles/master/tmux/.tmux.conf > ~/.tmux.conf
 
 echo "======"
 echo "Zsh + vim rc setup. Run source ~/.zshrc to see changes"
